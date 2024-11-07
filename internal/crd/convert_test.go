e99f15c5451546bf3139fcefb2a4ff1f789d32e7
Christopher Haar <christopher.haar@upbound.io>
2024-11-07 14:25:20 +0100

    feat(python): add defaults for apiVersion, kind for schemas
    Signed-off-by: Christopher Haar <christopher.haar@upbound.io>

--- internal/crd/convert_test.go
+++ internal/crd/convert_test.go
@@ -16,50 +16,36 @@ package crd
 
 import (
 	_ "embed"
-	"strings"
+	"path/filepath"
 	"testing"
 
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/kube-openapi/pkg/spec3"
+	"k8s.io/kube-openapi/pkg/validation/spec"
+	"sigs.k8s.io/yaml"
 )
 
 //go:embed testdata/template.fn.crossplane.io_kclinputs.yaml
 var testCRD []byte
 
 func TestConvertToOpenAPI(t *testing.T) {
+	// Define test cases
 	tests := []struct {
 		name        string
 		crdContent  []byte
 		expectedErr bool
-		validate    func(t *testing.T, fs afero.Fs, outputPath string)
 	}{
 		{
 			name:        "ValidCRDFromEmbed",
 			crdContent:  testCRD, // using the embedded CRD file
 			expectedErr: false,
-			validate: func(t *testing.T, fs afero.Fs, outputPath string) {
-				// Check if the file exists
-				require.True(t, strings.HasSuffix(outputPath, "template_fn_crossplane_io_v1beta1_kclinput.yaml"))
-
-				// Read the content from the file in-memory
-				openAPIContent, err := afero.ReadFile(fs, outputPath)
-				require.NoError(t, err)
-
-				// Validate some content in the generated OpenAPI schema
-				require.Contains(t, string(openAPIContent), "components")
-				require.Contains(t, string(openAPIContent), "schemas")
-				require.Contains(t, string(openAPIContent), "io.crossplane.fn.template.v1beta1.KCLInput")
-			},
 		},
 		{
-			name: "InvalidCRD",
-			crdContent: []byte(`
-invalid: crd content
-`),
+			name:        "InvalidCRD",
+			crdContent:  []byte(`invalid: crd content`),
 			expectedErr: true,
-			validate: func(t *testing.T, fs afero.Fs, outputPath string) {
-				// No validation needed because an error is expected
-			},
 		},
 		{
 			name: "CRDMissingVersion",
@@ -76,9 +62,6 @@ spec:
     plural: testresources
 `),
 			expectedErr: true,
-			validate: func(t *testing.T, fs afero.Fs, outputPath string) {
-				// No validation needed because an error is expected
-			},
 		},
 	}
 
@@ -97,8 +80,102 @@ spec:
 			}
 			require.NoError(t, err)
 
-			// Call the validation function if provided
-			tt.validate(t, fs, outputPath)
+			// Perform validation for the success case (only needed if no error was expected)
+			_, err = afero.Exists(fs, filepath.Join(outputPath, "template_fn_crossplane_io_v1beta1_kclinput.yaml"))
+			require.NoError(t, err)
+
+			// Read the content from the file in-memory
+			output, err := afero.ReadFile(fs, outputPath)
+			require.NoError(t, err)
+
+			var openapi *spec3.OpenAPI
+			err = yaml.Unmarshal(output, &openapi)
+			require.NoError(t, err)
+
+			apiVersionDefault := openapi.Components.Schemas["io.crossplane.fn.template.v1beta1.KCLInput"].SchemaProps.Properties["apiVersion"].Default
+			require.Equal(t, "template.fn.crossplane.io/v1beta1", apiVersionDefault, "The default value of apiVersion does not match the expected content")
+
+			kindDefault := openapi.Components.Schemas["io.crossplane.fn.template.v1beta1.KCLInput"].SchemaProps.Properties["kind"].Default
+			require.Equal(t, "KCLInput", kindDefault, "The default value of kind does not match the expected content")
+		})
+	}
+}
+
+func TestAddDefaultAPIVersionAndKind(t *testing.T) {
+	tests := []struct {
+		name               string
+		initialSchema      spec.Schema
+		gvk                schema.GroupVersionKind
+		expectedAPIVersion string
+		expectedKind       string
+	}{
+		{
+			name: "ApiVersionAndKind",
+			initialSchema: spec.Schema{
+				SchemaProps: spec.SchemaProps{
+					Properties: map[string]spec.Schema{
+						"apiVersion": {},
+						"kind":       {},
+					},
+				},
+			},
+			gvk:                schema.GroupVersionKind{Group: "example.com", Version: "v1", Kind: "ExampleKind"},
+			expectedAPIVersion: "example.com/v1",
+			expectedKind:       "ExampleKind",
+		},
+		{
+			name: "ApiVersion",
+			initialSchema: spec.Schema{
+				SchemaProps: spec.SchemaProps{
+					Properties: map[string]spec.Schema{
+						"apiVersion": {},
+					},
+				},
+			},
+			gvk:                schema.GroupVersionKind{Group: "example.com", Version: "v2", Kind: "AnotherKind"},
+			expectedAPIVersion: "example.com/v2",
+			expectedKind:       "",
+		},
+		{
+			name: "Kind",
+			initialSchema: spec.Schema{
+				SchemaProps: spec.SchemaProps{
+					Properties: map[string]spec.Schema{
+						"kind": {},
+					},
+				},
+			},
+			gvk:                schema.GroupVersionKind{Group: "example.com", Version: "v1alpha1", Kind: "SampleKind"},
+			expectedAPIVersion: "",
+			expectedKind:       "SampleKind",
+		},
+		{
+			name: "Nothing",
+			initialSchema: spec.Schema{
+				SchemaProps: spec.SchemaProps{
+					Properties: map[string]spec.Schema{},
+				},
+			},
+			gvk:                schema.GroupVersionKind{Group: "example.com", Version: "v1beta1", Kind: "NoDefaults"},
+			expectedAPIVersion: "",
+			expectedKind:       "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Call the function to add default values
+			addDefaultAPIVersionAndKind(&tt.initialSchema, tt.gvk)
+
+			// Check the apiVersion default if the property exists
+			if prop, ok := tt.initialSchema.Properties["apiVersion"]; ok {
+				require.Equal(t, tt.expectedAPIVersion, prop.Default, "apiVersion default should match expected value")
+			}
+
+			// Check the kind default if the property exists
+			if prop, ok := tt.initialSchema.Properties["kind"]; ok {
+				require.Equal(t, tt.expectedKind, prop.Default, "kind default should match expected value")
+			}
 		})
 	}
 }
