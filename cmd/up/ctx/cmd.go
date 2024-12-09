54e4c3286554988ca488636c053703d0a61d4a9c
Adam Wolfe Gordon <awg@upbound.io>
2024-12-09 11:20:23 -0700

    ctx: Rename the kubeconfig flag
    `up ctx` has a non-standard short form for `--kubeconfig`, `-f`, which we'd like
    to retain. It also accepts `-` as an argument to allow writing the kubeconfig to
    stdout.
    
    Since `--kubeconfig` is now part of the standard upbound flags, we need to
    rename `ctx`'s special version of it. Make it `--file` and treat it as an
    override for `--kubeconfig`.
    
    Signed-off-by: Adam Wolfe Gordon <awg@upbound.io>

--- cmd/up/ctx/cmd.go
+++ cmd/up/ctx/cmd.go
@@ -53,10 +53,10 @@ type Cmd struct {
 	// Common Upbound API configuration
 	Flags upbound.Flags `embed:""`
 
-	Argument    string `arg:""                                                help:".. to move to the parent, '-' for the previous context, '.' for the current context, or any relative path." optional:""`
-	Short       bool   `env:"UP_SHORT"                                        help:"Short output."                                                                                              name:"short"                             short:"s"`
-	KubeContext string `default:"upbound"                                     env:"UP_CONTEXT"                                                                                                  help:"Kubernetes context to operate on." name:"context"`
-	File        string `help:"Kubeconfig to modify when saving a new context" name:"kubeconfig"                                                                                                 short:"f"`
+	Argument    string `arg:""                                                                                                                       help:".. to move to the parent, '-' for the previous context, '.' for the current context, or any relative path." optional:""`
+	Short       bool   `env:"UP_SHORT"                                                                                                               help:"Short output."                                                                                              name:"short"                             short:"s"`
+	KubeContext string `default:"upbound"                                                                                                            env:"UP_CONTEXT"                                                                                                  help:"Kubernetes context to operate on." name:"context"`
+	File        string `help:"Kubeconfig to modify when saving a new context. Overrides the --kubeconfig flag. Use '-' to write to standard output." short:"f"`
 }
 
 // AfterApply processes flags and sets defaults.
