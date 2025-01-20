4a84a74cb771bb96243e3cc316931365f92e7df1
Adam Wolfe Gordon <awg@upbound.io>
2025-01-20 13:19:50 -0700

    projects: Move repository determination to internal package
    This will be used for e2e tests as well as `up project run`, so put it in a
    shared package.
    
    Signed-off-by: Adam Wolfe Gordon <awg@upbound.io>

--- cmd/up/project/run/run.go
+++ cmd/up/project/run/run.go
@@ -19,7 +19,6 @@ import (
 	"context"
 	"net/http"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/alecthomas/kong"
@@ -221,7 +220,7 @@ func (c *Cmd) Run(ctx context.Context, upCtx *upbound.Context) error {
 		return err
 	}
 
-	c.Repository, err = determineRepository(upCtx, proj, c.Repository)
+	c.Repository, err = project.DetermineRepository(upCtx, proj, c.Repository)
 	if err != nil {
 		return err
 	}
@@ -318,57 +317,6 @@ func (c *Cmd) Run(ctx context.Context, upCtx *upbound.Context) error {
 	}
 
 	return nil
-}
-
-func determineRepository(upCtx *upbound.Context, proj *v1alpha1.Project, repoArgument string) (string, error) {
-	defaultRegistry := upCtx.RegistryEndpoint.Host
-
-	// If the user explicitly set the repository, use it, but if it's in the
-	// Upbound registry check that the repository is owned by the org matching
-	// their current profile.
-	if repoArgument != "" {
-		ref, err := name.NewRepository(repoArgument, name.WithDefaultRegistry(defaultRegistry))
-		if err != nil {
-			return "", errors.Wrap(err, "failed to parse repository")
-		}
-
-		if ref.RegistryStr() == defaultRegistry {
-			_, org, _, err := upbound.ParseRepository(repoArgument, upCtx.RegistryEndpoint.Host)
-			if err != nil {
-				return "", err
-			}
-
-			if org != upCtx.Organization {
-				return "", errors.New("specified repository does not belong to your current organization; use `up profile use` to select a different organization")
-			}
-		}
-
-		// Make sure c.Repository is fully qualified.
-		return ref.String(), nil
-	}
-
-	// If the user didn't explicitly set a repository, and the project's
-	// repository is in the Upbound registry but not owned by the user's
-	// organization, construct a new repository name that is owned by them. This
-	// gives users the maximum chance of `up project run` Just Working when they
-	// check out an example repo.
-
-	ref, err := name.NewRepository(proj.Spec.Repository, name.WithDefaultRegistry(defaultRegistry))
-	if err != nil {
-		return "", errors.Wrap(err, "failed to parse repository")
-	}
-
-	if ref.RegistryStr() == defaultRegistry {
-		_, _, repoName, err := upbound.ParseRepository(proj.Spec.Repository, upCtx.RegistryEndpoint.Host)
-		if err != nil {
-			return "", err
-		}
-
-		// Always use the host and org from the context
-		return strings.Join([]string{upCtx.RegistryEndpoint.Host, upCtx.Organization, repoName}, "/"), nil
-	}
-
-	return proj.Spec.Repository, nil
 }
 
 // getCurrentSpaceNavigation derives the state of the current navigation using
