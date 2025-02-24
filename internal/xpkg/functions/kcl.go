271968b88709c25ac7e693299c687ecdf8e0c442
Adam Wolfe Gordon <awg@upbound.io>
2025-02-24 15:34:37 -0700

    xpkg: Split function builders into their own files
    Signed-off-by: Adam Wolfe Gordon <awg@upbound.io>

---
+++ internal/xpkg/functions/kcl.go
@@ -0,0 +1,189 @@
+// Copyright 2025 Upbound Inc.
+// All rights reserved
+
+package functions
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"slices"
+
+	"github.com/google/go-containerregistry/pkg/name"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/empty"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"github.com/spf13/afero"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/crossplane/crossplane-runtime/pkg/errors"
+
+	"github.com/upbound/up/internal/filesystem"
+	"github.com/upbound/up/internal/xpkg"
+)
+
+const (
+	crossplaneFunctionRunnerUID = 2000
+	crossplaneFunctionRunnerGID = 2000
+)
+
+// kclBuilder builds functions written in KCL by injecting their code into a
+// function-kcl base image.
+type kclBuilder struct {
+	baseImage string
+	transport http.RoundTripper
+}
+
+func (b *kclBuilder) Name() string {
+	return "kcl"
+}
+
+func (b *kclBuilder) match(fromFS afero.Fs) (bool, error) {
+	return afero.Exists(fromFS, "kcl.mod")
+}
+
+func (b *kclBuilder) Build(ctx context.Context, fromFS afero.Fs, architectures []string, osBasePath string) ([]v1.Image, error) {
+	baseRef, err := name.NewTag(b.baseImage)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse KCL base image tag")
+	}
+
+	images := make([]v1.Image, len(architectures))
+	eg, _ := errgroup.WithContext(ctx)
+	for i, arch := range architectures {
+		eg.Go(func() error {
+			baseImg, err := baseImageForArch(baseRef, arch, b.transport)
+			if err != nil {
+				return errors.Wrap(err, "failed to fetch KCL base image")
+			}
+
+			src, err := filesystem.FSToTar(fromFS, "/src",
+				filesystem.WithSymlinkBasePath(osBasePath),
+				// The KCL base function implementation requires that the source
+				// files (specifically, the kcl.mod.lock) be writable. Make them
+				// owned by the UID/GID that crossplane uses to run function
+				// pods.
+				filesystem.WithUIDOverride(crossplaneFunctionRunnerUID),
+				filesystem.WithGIDOverride(crossplaneFunctionRunnerGID),
+			)
+			if err != nil {
+				return errors.Wrap(err, "failed to tar layer contents")
+			}
+
+			codeLayer, err := tarball.LayerFromOpener(func() (io.ReadCloser, error) {
+				return io.NopCloser(bytes.NewReader(src)), nil
+			})
+			if err != nil {
+				return errors.Wrap(err, "failed to create code layer")
+			}
+
+			img, err := mutate.AppendLayers(baseImg, codeLayer)
+			if err != nil {
+				return errors.Wrap(err, "failed to add code to image")
+			}
+
+			// Set the default source to match our source directory.
+			img, err = setImageEnvvar(img, "FUNCTION_KCL_DEFAULT_SOURCE", "/src")
+			if err != nil {
+				return errors.Wrap(err, "failed to configure KCL source path")
+			}
+
+			images[i] = img
+			return nil
+		})
+	}
+
+	return images, eg.Wait()
+}
+
+// baseImageForArch pulls the image with the given ref, and returns a version of
+// it suitable for use as a function base image. Specifically, the package
+// layer, examples layer, and schema layers will be removed if present. Note
+// that layers in the returned image will refer to the remote and be pulled only
+// if they are read by the caller.
+func baseImageForArch(ref name.Reference, arch string, transport http.RoundTripper) (v1.Image, error) {
+	img, err := remote.Image(ref, remote.WithPlatform(v1.Platform{
+		OS:           "linux",
+		Architecture: arch,
+	}), remote.WithTransport(transport))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to pull image")
+	}
+
+	cfg, err := img.ConfigFile()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get config from image")
+	}
+	if cfg.Architecture != arch {
+		return nil, errors.Errorf("image not available for architecture %q", arch)
+	}
+
+	// Remove the package layer and schema layers if present.
+	mfst, err := img.Manifest()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get manifest from image")
+	}
+	baseImage := empty.Image
+	// The RootFS contains a list of layers; since we're removing layers we need
+	// to clear it out. It will be rebuilt by the mutate package.
+	cfg.RootFS = v1.RootFS{}
+	cfg.History = nil
+	baseImage, err = mutate.ConfigFile(baseImage, cfg)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to add configuration to base image")
+	}
+	for _, desc := range mfst.Layers {
+		if isNonBaseLayer(desc) {
+			continue
+		}
+		l, err := img.LayerByDigest(desc.Digest)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to get layer from image")
+		}
+		baseImage, err = mutate.AppendLayers(baseImage, l)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to add layer to base image")
+		}
+	}
+
+	return baseImage, nil
+}
+
+func isNonBaseLayer(desc v1.Descriptor) bool {
+	nonBaseLayerAnns := []string{
+		xpkg.PackageAnnotation,
+		xpkg.ExamplesAnnotation,
+		xpkg.SchemaKclAnnotation,
+		xpkg.SchemaPythonAnnotation,
+	}
+
+	ann := desc.Annotations[xpkg.AnnotationKey]
+	return slices.Contains(nonBaseLayerAnns, ann)
+}
+
+func setImageEnvvar(image v1.Image, key string, value string) (v1.Image, error) {
+	cfgFile, err := image.ConfigFile()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get config file")
+	}
+	cfg := cfgFile.Config
+	cfg.Env = append(cfg.Env, fmt.Sprintf("%s=%s", key, value))
+
+	image, err = mutate.Config(image, cfg)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to set config")
+	}
+
+	return image, nil
+}
+
+func newKCLBuilder() *kclBuilder {
+	return &kclBuilder{
+		baseImage: "xpkg.upbound.io/upbound/function-kcl-base:v0.11.2-up.1",
+		transport: http.DefaultTransport,
+	}
+}
---
+++ internal/xpkg/functions/python.go
@@ -0,0 +1,97 @@
+// Copyright 2025 Upbound Inc.
+// All rights reserved
+
+package functions
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+
+	"github.com/google/go-containerregistry/pkg/name"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"github.com/spf13/afero"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/crossplane/crossplane-runtime/pkg/errors"
+
+	"github.com/upbound/up/internal/filesystem"
+)
+
+// pythonBuilder builds functions written in python by injecting their code into a
+// function-python base image.
+type pythonBuilder struct {
+	baseImage   string
+	packagePath string
+	transport   http.RoundTripper
+}
+
+func (b *pythonBuilder) Name() string {
+	return "python"
+}
+
+func (b *pythonBuilder) match(fromFS afero.Fs) (bool, error) {
+	// More reliable than requirements.txt, which is optional.
+	return afero.Exists(fromFS, "main.py")
+}
+
+func (b *pythonBuilder) Build(ctx context.Context, fromFS afero.Fs, architectures []string, osBasePath string) ([]v1.Image, error) {
+	baseRef, err := name.NewTag(b.baseImage)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse python base image tag")
+	}
+
+	images := make([]v1.Image, len(architectures))
+	eg, _ := errgroup.WithContext(ctx)
+	for i, arch := range architectures {
+		eg.Go(func() error {
+			baseImg, err := remote.Image(baseRef, remote.WithPlatform(v1.Platform{
+				OS:           "linux",
+				Architecture: arch,
+			}), remote.WithTransport(b.transport))
+			if err != nil {
+				return errors.Wrap(err, "failed to fetch python base image")
+			}
+
+			src, err := filesystem.FSToTar(fromFS, b.packagePath,
+				filesystem.WithSymlinkBasePath(osBasePath),
+			)
+			if err != nil {
+				return errors.Wrap(err, "failed to tar layer contents")
+			}
+
+			codeLayer, err := tarball.LayerFromOpener(func() (io.ReadCloser, error) {
+				return io.NopCloser(bytes.NewReader(src)), nil
+			})
+			if err != nil {
+				return errors.Wrap(err, "failed to create code layer")
+			}
+
+			img, err := mutate.AppendLayers(baseImg, codeLayer)
+			if err != nil {
+				return errors.Wrap(err, "failed to add code to image")
+			}
+
+			images[i] = img
+			return nil
+		})
+	}
+
+	return images, eg.Wait()
+}
+
+func newPythonBuilder() *pythonBuilder {
+	return &pythonBuilder{
+		// TODO(negz): Should this be hardcoded?
+		baseImage: "xpkg.upbound.io/upbound/function-interpreter-python:v0.3.0",
+
+		// TODO(negz): This'll need to change if function-interpreter-python is
+		// updated to a distroless base layer that uses a newer Python version.
+		packagePath: "/venv/fn/lib/python3.11/site-packages/function",
+		transport:   http.DefaultTransport,
+	}
+}
