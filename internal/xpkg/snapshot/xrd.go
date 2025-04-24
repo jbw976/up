6e9ff68be4c7054b07f4f8f6364246a52d9d4e9f
Christopher Haar <christopher.haar@upbound.io>
2025-04-24 15:58:11 +0200

    stlye(docs): add comment
    Signed-off-by: Christopher Haar <christopher.haar@upbound.io>

--- internal/xpkg/snapshot/xrd.go
+++ internal/xpkg/snapshot/xrd.go
@@ -122,6 +122,9 @@ func (v *XRDSchemaValidator) validate(ctx context.Context, xrd *xpextv1.Composit
 // validateOpenAPIV3Schema validates the spec.versions[*].schema.openAPIV3Schema
 // section of the given XRD definition.
 func validateOpenAPIV3Schema(ctx context.Context, xrd *xpextv1.CompositeResourceDefinition) []error {
+	// we need to set UID
+	// ValidateCustomResourceDefinition is checking ownerReference and UID
+	// ref: https://github.com/kubernetes/apimachinery/blob/6a84120b17236460f404ba6486926f62cbf733dd/pkg/api/validation/objectmeta.go#L82-L84
 	if xrd.UID == "" {
 		xrd.UID = types.UID("dummy-uid")
 	}
