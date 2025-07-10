d2e3c89dbc201281ffe5944eecf2ce07a0d40bff
Adam Wolfe Gordon <awg@upbound.io>
2025-07-10 16:00:20 -0600

    uxp: Implement `up uxp license apply`
    The `license apply` command reads a base64-encoded license from a file, creates
    a secret containing the license data, then updates the license CR to refer to
    the secret. For now, at least, it does not validate the license before applying
    it.
    
    Toward #938
    
    Signed-off-by: Adam Wolfe Gordon <awg@upbound.io>

--- cmd/up/uxp/license/license.go
+++ cmd/up/uxp/license/license.go
@@ -6,13 +6,18 @@ package license
 
 import (
 	"github.com/alecthomas/kong"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"github.com/crossplane/crossplane-runtime/pkg/errors"
+
+	"github.com/upbound/controller-manager/apis/licensing/v1alpha1"
 	"github.com/upbound/up/internal/upbound"
 )
 
 // Cmd is the `up uxp license` command.
 type Cmd struct {
-	Show showCmd `cmd:"" help:"Show the UXP license for a control plane."`
+	Show  showCmd  `cmd:"" help:"Show the UXP license for a control plane."`
+	Apply applyCmd `cmd:"" help:"Apply a UXP license to a control plane."`
 
 	Flags upbound.Flags `embed:""`
 }
@@ -25,7 +30,16 @@ func (c *Cmd) AfterApply(kongCtx *kong.Context) error {
 	}
 	upCtx.SetupLogging()
 
-	kongCtx.Bind(upCtx)
+	cl, err := upCtx.BuildCurrentContextClient()
+	if err != nil {
+		return errors.Wrap(err, "failed to get kube client")
+	}
+
+	if err := v1alpha1.AddToScheme(cl.Scheme()); err != nil {
+		return errors.Wrap(err, "failed to add license types to scheme")
+	}
+
+	kongCtx.BindTo(cl, (*client.Client)(nil))
 
 	return nil
 }
