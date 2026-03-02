ffb4b8d1edb85dcf72bc3c33927e4d8556ce39a4
Philippe Scorsolini <5697904+phisco@users.noreply.github.com>
2026-03-02 13:27:30 +0100

    review
    Signed-off-by: Philippe Scorsolini <5697904+phisco@users.noreply.github.com>

--- cmd/up/uxp/version.go
+++ cmd/up/uxp/version.go
@@ -14,14 +14,11 @@ import (
 
 type version string
 
-//nolint:gochecknoglobals // Would make this a const if we could.
-var uxpV2Version = semver.MustParse("2.0.0-up.0")
-
 // Validate implements the kong Validatable interface, see https://github.com/alecthomas/kong?tab=readme-ov-file#validation.
 // Unfortunately it's not an exposed interface, so we can't enforce version to implement it at build time.
-func (v *version) Validate() error {
+func (v version) Validate() error {
 	// version should not start with a v
-	vs := string(*v)
+	vs := string(v)
 	if strings.HasPrefix(vs, "v") {
 		return fmt.Errorf("versions should not start with a v: %s", vs)
 	}
@@ -29,7 +26,7 @@ func (v *version) Validate() error {
 	if err != nil {
 		return errors.Wrapf(err, "unable to parse version %s", vs)
 	}
-	if pv.LT(uxpV2Version) {
+	if uxpV2Version := semver.MustParse("2.0.0-up.0"); pv.LT(uxpV2Version) {
 		return fmt.Errorf("this command only supports UXP versions >= %v, please use the helm chart directly if you want to install UXP v1", uxpV2Version)
 	}
 	return nil
